38745787d19814eca4767f65952f74d67b83d0e3
AidoRiko <Zelenevsky.nikola@gmail.com>
2017-06-11 22:57:51 +0300

    Update Client.java

--- MyStorage/src/main/java/by/zelenevsky/rest/Client.java
+++ MyStorage/src/main/java/by/zelenevsky/rest/Client.java
@@ -1,49 +1,45 @@
 package by.zelenevsky.rest;
 
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import by.zelenevsky.dto.GuitarsDto;
-import com.google.gson.Gson;
+import by.zelenevsky.execute.DataConnecter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Iterator;
-import java.util.Set;
 
 @Path("guitar")
 public class Client {
 
-    @GET
-    @Path ("getAll")
-    @Produces("application/json")
-    public String getAllGuitars() {
-        Guitar.setGuitarsList(1, "Gibson", "Blue");
-        Guitar.setGuitarsList(2, "Cort", "Accord");
-        Guitar.setGuitarsList(3, "Boobs", "More Boobs");
-        Set<GuitarsDto> guitars = Guitar.getGuitarsList();
-        Gson gson = new Gson();
-        String jasonGuitar = gson.toJson(guitars);
-        return jasonGuitar;
-    }
-
+    public static List<GuitarsDto> guitarsList = new ArrayList<GuitarsDto>();
 
     @GET
-    @Path("GET/{model}")
+    @Path ("connect")
     @Produces(MediaType.APPLICATION_JSON)
-    public  String getGuitarByModel(@PathParam("model") String model) {
-        Guitar.setGuitarsList(1, "Gibson", "Blue");
-        Guitar.setGuitarsList(2, "Cort", "Accord");
-        Guitar.setGuitarsList(3, "Boobs", "More Boobs");
-        GuitarsDto guitar = Guitar.findGuitarByModel(model);
-        String json = new Gson().toJson(guitar);
-        return json;
+    public String getConnection(){
+        DataConnecter.fillList();
+        return "Connection finished succesfull!";
     }
 
-    @DELETE
-    @Path("DELETE/{model}")
-    public void deleteGuitar(@PathParam("model") String model) {
-        if (Guitar.deleteGuitarByModel(model)!= true) {
-            throw new RuntimeException("can't delete guitar model: " + model);
+    @GET
+    @Path ("print/guitars")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<GuitarsDto> printAll() {
+        return guitarsList;
+    }
+
+    @GET
+    @Path ("print/model/{model}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public GuitarsDto printModel(@PathParam ("model") String model){
+        GuitarsDto myGuitar = new GuitarsDto();
+        for (GuitarsDto guitar : guitarsList){
+            myGuitar = guitar;
+            if (myGuitar.getModelName() == model)
+                return myGuitar;
         }
+        return myGuitar;
     }
 }
