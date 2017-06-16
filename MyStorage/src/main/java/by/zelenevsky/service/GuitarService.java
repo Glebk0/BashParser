02e9426864108f53b28539bdf1f53addd75beae6
AidoRiko <Zelenevsky.nikola@gmail.com>
2017-06-16 22:26:39 +0300

    Update GuitarService.java

--- MyStorage/src/main/java/by/zelenevsky/service/GuitarService.java
+++ MyStorage/src/main/java/by/zelenevsky/service/GuitarService.java
@@ -6,158 +6,136 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class GuitarService {
 
     private static String url = "jdbc:mysql://localhost:3306/guitars";
     private static String user = "root";
     private static String password = "123456";
     private static Connection connection;
-    private static Statement statement;
-    private static ResultSet resultSet;
+    private static PreparedStatement statement;
 
-    private static List<GuitarsDto> getGuitars(List<GuitarsDto> guitarsList){
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select availability.id, brand.name, model.name, availability.serial_number " +
-                    "from brand, model, availability " +
-                    "where availability.model_id = model.id and model.brand_id = brand.id and availability.status = 1");
-            while (resultSet.next()) {
+    ResultSetHandler handler = new ResultSetHandler<GuitarsDto>() {
+        @Override
+        public List<GuitarsDto> guitarsHandler(ResultSet rs, List<GuitarsDto> guitarsList) throws SQLException {
+            while (rs.next()) {
                 GuitarsDto guitarsDto = new GuitarsDto();
-                guitarsDto.setId(resultSet.getLong(1));
-                guitarsDto.setBrandName(resultSet.getString(2));
-                guitarsDto.setModelName(resultSet.getString(3));
-                guitarsDto.setSerialNumber(resultSet.getLong(4));
+                guitarsDto.setId(rs.getLong("id"));
+                guitarsDto.setBrandName(rs.getString("brandName"));
+                guitarsDto.setModelName(rs.getString("modelName"));
+                guitarsDto.setSerialNumber(rs.getLong("serialNumber"));
                 guitarsList.add(guitarsDto);
             }
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException se) { /*can't do anything */ }
+            return guitarsList;
         }
+        @Override
+        public List<ModelsDto> modelsHandler(ResultSet rs, List<ModelsDto> modelsList) throws SQLException {
+            while (rs.next()) {
+                ModelsDto modelsDto = new ModelsDto();
+                modelsDto.setId(rs.getLong("id"));
+                modelsDto.setModelName(rs.getString("modelName"));
+                modelsDto.setSerialNumber(rs.getLong("serialNumber"));
+                modelsDto.setPrice(rs.getDouble("price"));
+                modelsList.add(modelsDto);
+            }
+            return modelsList;
+        }
+
+        @Override
+        public ModelsDto modelHandler(ResultSet rs) throws SQLException {
+            ModelsDto modelsDto = new ModelsDto();
+            while (rs.next()) {
+                modelsDto.setId(rs.getLong("id"));
+                modelsDto.setModelName(rs.getString("modelName"));
+                modelsDto.setSerialNumber(rs.getLong("serialNumber"));
+                modelsDto.setPrice(rs.getDouble("price"));
+            }
+            return modelsDto;
+        }
+
+        @Override
+        public List<QuantityDto> quantityHandler(ResultSet rs, List<QuantityDto> modelsList) throws SQLException {
+            while (rs.next()) {
+                QuantityDto quantityDto = new QuantityDto();
+                quantityDto.setId(rs.getLong(1));
+                quantityDto.setModelName(rs.getString(2));
+                quantityDto.setQuantity(rs.getLong(3));
+                modelsList.add(quantityDto);
+            }
+            return modelsList;
+        }
+
+    };
+
+    private static Connection getConnection() throws SQLException {
+            try {
+                connection = DriverManager.getConnection(url, user, password);
+                return connection;
+            } catch (SQLException sqlEx) {
+                sqlEx.printStackTrace();
+            }
+        return connection;
+    }
+
+    private static void closeConnection() throws SQLException{
+        try{
+            connection.close();
+            statement.close();
+        } catch (SQLException se){}
+    }
+
+    public List<GuitarsDto> findGuitars() throws SQLException {
+        List <GuitarsDto> guitarsList = new ArrayList<GuitarsDto>();
+        getConnection();
+        statement = connection.prepareStatement("select a.id id, b.name brandName, m.name modelName, a.serial_number serialNumber " +
+                "from brand b, model m, availability a " +
+                "where a.model_id = m.id and m.brand_id = b.id and a.status = 1");
+        handler.guitarsHandler(statement.executeQuery(), guitarsList);
+        System.out.println(guitarsList);
+        closeConnection();
         return guitarsList;
     }
 
-    private static List<ModelsDto> getModels(List<ModelsDto> modelsList, String modelName){
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select availability.id, model.name, availability.serial_number, model.price " +
-                    "from  model, availability  " +
-                    "where availability.model_id = model.id and  availability.status = 1 and model.name = '" +  modelName + "'");
-            while (resultSet.next()) {
-                ModelsDto modelsDto = new ModelsDto();
-                modelsDto.setId(resultSet.getLong(1));
-                modelsDto.setModelName(resultSet.getString(2));
-                modelsDto.setSerialNumber(resultSet.getLong(3));
-                modelsDto.setPrice(resultSet.getDouble(4));
-                modelsList.add(modelsDto);
-            }
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException se) { /*can't do anything */ }
-        }
-        return modelsList;
-    }
-
-    private static List<ModelsDto> getModel(List<ModelsDto> modelsList, long serialNumber){
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select availability.id, model.name, availability.serial_number, model.price " +
-                    "from  model, availability  " +
-                    "where availability.model_id = model.id and availability.serial_number = " + serialNumber);
-            while (resultSet.next()) {
-                ModelsDto modelsDto = new ModelsDto();
-                modelsDto.setId(resultSet.getLong(1));
-                modelsDto.setModelName(resultSet.getString(2));
-                modelsDto.setSerialNumber(resultSet.getLong(3));
-                modelsDto.setPrice(resultSet.getDouble(4));
-                modelsList.add(modelsDto);
-            }
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException se) { /*can't do anything */ }
-        }
-        return modelsList;
-    }
-
-    public static List<GuitarsDto> findGuitars(){
-        List<GuitarsDto> guitarsList = new ArrayList<GuitarsDto>();
-        getGuitars(guitarsList);
-        return guitarsList;
-    }
-
-    public static ModelsDto findModel(long serialNumber){
+    public List<ModelsDto> findModels(long modelId) throws SQLException {
         List<ModelsDto> modelsList = new ArrayList<ModelsDto>();
-        getModel(modelsList, serialNumber);
-        ModelsDto myModel = new ModelsDto();
-        for (ModelsDto guitar : modelsList){
-            myModel = guitar;
-            if (myModel.getSerialNumber() == serialNumber)
-                return myModel;
-        }
-        return myModel;
-    }
-
-    public static List<ModelsDto> findModels(String modelName){
-        List<ModelsDto> modelsList = new ArrayList<ModelsDto>();
-        getModels(modelsList, modelName);
+        getConnection();
+        statement = connection.prepareStatement("select a.id id, m.name modelName, a.serial_number serialNumber, a.price price " +
+                "from  model m, availability a " +
+                "where a.model_id = m.id and  a.status = 1 and m.name = '" +  modelId + "'");
+        handler.modelsHandler(statement.executeQuery(), modelsList);
+        closeConnection();
         return modelsList;
     }
 
-    public static String sellGuitar(long serialNubmer){
-        String result = "Not Sold!";
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.createStatement();
-            statement.execute("UPDATE availability SET status = '0' WHERE availability.serialNumber = " + serialNubmer + ";");
-            result = "Sold!";
-        } catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException se) { /*can't do anything */ }
-        }
+    public ModelsDto findModel(long serialNumber) throws SQLException{
+        ModelsDto modelsDto = new ModelsDto();
+        getConnection();
+            statement = connection.prepareStatement("select a.id id, m.name modelName, a.serial_number serialNumber, a.price price " +
+                    "from  model m, availability a " +
+                    "where a.model_id = m.id  and a.serial_number = '" +  serialNumber + "'");
+            modelsDto = handler.modelHandler(statement.executeQuery());
+        closeConnection();
+        return modelsDto;
+    }
+
+    public String sellGuitar(long serialNubmer) throws SQLException {
+        String result = "The guitar is already sold!";
+        getConnection();
+            statement = connection.prepareStatement("UPDATE availability SET status = '0' WHERE serial_number = " + serialNubmer + " and status = 1");
+            statement.execute();
+            result = "The sale was completed successfully!";
+        closeConnection();
         return result;
     }
 
-    public static List<QuantityDto> guitarsQuantity(){
+    public List<QuantityDto> guitarsQuantity() throws SQLException {
         List<QuantityDto> modelsList = new ArrayList<QuantityDto>();
-        try{
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT model.id, model.name, count(availability.model_id) " +
-                    "FROM model, availability " +
-                    "WHERE availability.model_id = model.ID and status = 1 " +
-                    "GROUP BY model.id");
-            while (resultSet.next()) {
-                QuantityDto quantityDto = new QuantityDto();
-                quantityDto.setId(resultSet.getLong(1));
-                quantityDto.setModelName(resultSet.getString(2));
-                quantityDto.setQuantity(resultSet.getLong(3));
-                modelsList.add(quantityDto);
-            }
-        }
-        catch (SQLException sqlEx) {
-            sqlEx.printStackTrace();
-        }
-        finally {
-        try {
-            connection.close();
-        } catch (SQLException se) { /*can't do anything */ }
-    }
+        getConnection();
+        statement = connection.prepareStatement("SELECT model.id, model.name, count(availability.model_id) " +
+                "FROM model, availability " +
+                "WHERE availability.model_id = model.ID and status = 1 " +
+                "GROUP BY model.id");
+        handler.quantityHandler(statement.executeQuery(), modelsList);
+        closeConnection();
     return modelsList;
     }
-
 }
