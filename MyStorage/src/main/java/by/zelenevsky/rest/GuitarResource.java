05f68e81abe20729148cd90d09cb05a0cc4cf02b
AidoRiko <Zelenevsky.nikola@gmail.com>
2017-06-19 18:34:05 +0300

    Update GuitarResource.java

--- MyStorage/src/main/java/by/zelenevsky/rest/GuitarResource.java
+++ MyStorage/src/main/java/by/zelenevsky/rest/GuitarResource.java
@@ -6,37 +6,45 @@ import javax.ws.rs.core.MediaType;
 import by.zelenevsky.dto.*;
 import by.zelenevsky.service.GuitarService;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 
 @Path("storage")
 public class GuitarResource {
 
+    GuitarService guitarService = new GuitarService();
     @GET
     @Path ("/guitars")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<GuitarsDto> getGuitars() {
-        return GuitarService.findGuitars();
+    public List<GuitarsDto> getGuitars() throws SQLException {
+        return guitarService.findGuitars();
     }
 
     @GET
-    @Path("/guitars/{model}")
+    @Path("/guitars/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<ModelsDto> findModels(@PathParam ("model") String model){
-        return GuitarService.findModels(model);
+    public List<ModelsDto> findModels(@PathParam ("id") long id) throws SQLException {
+        return guitarService.findModels(id);
     }
 
     @GET
     @Path ("/models/{serialNumber}")
     @Produces(MediaType.APPLICATION_JSON)
-    public ModelsDto getModel(@PathParam ("serialNumber") long serialNumber){
-        return GuitarService.findModel(serialNumber);
+    public List<ModelsDto> getModel(@PathParam ("serialNumber") long serialNumber) throws SQLException {
+        return guitarService.findModel(serialNumber);
     }
 
     @GET
     @Path ("/quantity")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<QuantityDto> guitarsQuantity(){
-        return GuitarService.guitarsQuantity();
+    public List<QuantityDto> guitarsQuantity() throws SQLException {
+        return guitarService.guitarsQuantity();
+    }
+
+    @GET
+    @Path ("/sell/{serialNumber}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String sellGuitar(@PathParam ("serialNumber") long serialNumber) throws SQLException {
+        return guitarService.sellGuitar(serialNumber);
     }
 }
